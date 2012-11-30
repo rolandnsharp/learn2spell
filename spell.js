e6edf446b24813393b957c852193e5ace79790af
rolandnsharp <rolandnsharp@gmail.com>
2012-11-30 23:23:39 +1000

    color red

--- spell.js
+++ spell.js
@@ -4,12 +4,23 @@ var word = ["dog","cat","fish","frog","spider","bourgeois"];
 var i = 0;
 
 
+
+
+
+
 $(document).ready(function(){
 
+    $('p').append(word[i]);
 
-			$('p').append(word[i]);
+
+
+
+
+
 
 $(document).keypress(function(e) {
+
+
 
     if(e.which == 13) {
     	var spell = document.getElementById('spellbox');
@@ -22,21 +33,15 @@ $(document).keypress(function(e) {
 
 			document.getElementById("spellbox").value = "";
 			
-			 }
+			 } else {
+                $('#spellbox').animate({backgroundColor: '#e01432'}).delay(40).animate({backgroundColor: '#ffffff'});}
     }
-});
-
-
-
-
-
-
-
-
 
 
 
 });
 
+
+});
 
 
