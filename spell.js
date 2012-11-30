af75a5e1226387fc40f1e0da3383d6830660ae7d
rolandnsharp <rolandnsharp@gmail.com>
2012-11-30 22:21:53 +1000

    color

--- spell.js
+++ spell.js
@@ -13,11 +13,13 @@ $(document).keypress(function(e) {
 
     if(e.which == 13) {
     	var spell = document.getElementById('spellbox');
-    	console.log(spell.value);
+    	
     	if (spell.value===word[i]) {
     		i = i+1;
     		$('p').empty();
 			$('p').append(word[i]);
+            $('#spellbox').animate({backgroundColor: '#5bd642'}).delay(40).animate({backgroundColor: '#ffffff'});
+
 			document.getElementById("spellbox").value = "";
 			
 			 }
